88280e5aa5da7617fe6bf492f4ced779b6640e8f
Josh Anderson <111634244+Weaponboy@users.noreply.github.com>
2024-02-04 16:21:21 +0200

    2+2 auto test ready

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry/Pathing/PathGeneration/Blue_Points_Overlap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry/Pathing/PathGeneration/Blue_Points_Overlap.java
@@ -18,7 +18,7 @@ public interface Blue_Points_Overlap {
     //second segment
     Vector2D CS2F = CE1F;
     Vector2D CC2F = new Vector2D(getRealCoords(84), getRealCoords(181));
-    Vector2D CE2F = new Vector2D(getRealCoords(41), getRealCoords(148));
+    Vector2D CE2F = new Vector2D(getRealCoords(38), getRealCoords(148));
 
 //    /**second position*/
 //    Vector2D CS1S = new Vector2D(getRealCoords(300), getRealCoords(90));
