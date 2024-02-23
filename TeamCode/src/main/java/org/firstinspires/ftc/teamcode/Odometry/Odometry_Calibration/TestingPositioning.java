a440323be29893ba3c97ef3a703dbe5eb823a378
Josh Anderson <111634244+Weaponboy@users.noreply.github.com>
2024-02-23 17:43:31 +0200

    Pathing testing full power

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry/Odometry_Calibration/TestingPositioning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry/Odometry_Calibration/TestingPositioning.java
@@ -1,10 +1,17 @@
 package org.firstinspires.ftc.teamcode.Odometry.Odometry_Calibration;
 
+import static org.firstinspires.ftc.teamcode.Constants_and_Setpoints.Constants.botHeading;
+
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.teamcode.hardware._.Odometry;
 
 @TeleOp
@@ -16,9 +23,23 @@ public class TestingPositioning extends OpMode {
 
     Telemetry dashboardTelemetry = dashboard.getTelemetry();
 
+    public BNO055IMU imu = null;
+
+    Orientation YawAngle;
+
     @Override
     public void init() {
         odometry.init(hardwareMap);
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        imu.initialize(parameters);
     }
 
     @Override
@@ -31,7 +52,9 @@ public class TestingPositioning extends OpMode {
 
         telemetry.addData("heading", odometry.heading);
 
-        telemetry.addData("heading raw", odometry.headingRaw);
+        YawAngle = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        telemetry.addData("heading raw", YawAngle.firstAngle);
 
         telemetry.update();
     }
